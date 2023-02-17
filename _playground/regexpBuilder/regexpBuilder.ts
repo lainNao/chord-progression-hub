7d5ee330925cc5f6ad174f1e7b2a72616a4eb2f6
nao <nao>
2023-02-18 01:48:03 +0900

    リファクタ

--- _playground/regexpBuilder/regexpBuilder.ts
+++ _playground/regexpBuilder/regexpBuilder.ts
@@ -13,42 +13,64 @@ export const matchWholeString = (str: string) => `^${str}$`;
 // オプショナルにする
 export const optional = (str: string) => `(${str})?`;
 
-export const zeroOrMore = (str: string) => `(${str})*`;
-
-//一つ以上必須にする
-//TODO: これだけがリッチなのおかしい。zeroOrMoreでもこのリッチ差ほしいはずなので
-export const oneOrMore = (str: string, option?: {
-  delimiter: string
-} | {
-  parentheses: string | {
-    left: string;
-    right: string;
-  }
-}) => {
-  if (!option){ 
-    return `(${str})+`
+//0か1つ以上必須にする
+export const orMore = (
+  str: string,
+  count: 0 | 1,
+  option?:
+    | {
+        delimiter: string;
+      }
+    | {
+        parentheses:
+          | string
+          | {
+              left: string;
+              right: string;
+            };
+      }
+) => {
+  const repeater = count === 0 ? "*" : "+";
+  if (!option) {
+    return `(${str})${repeater}`;
   }
   if ("delimiter" in option) {
-    return `(${str})(${option.delimiter}(${str}))+`
+    return `(${str})(${option.delimiter}(${str}))${repeater}`;
   }
   if ("parentheses" in option) {
-    const left = typeof option.parentheses === "string" ? option.parentheses : option.parentheses.left;
-    const right = typeof option.parentheses === "string" ? option.parentheses : option.parentheses.right;
-    return `${left}(${str}(${right}))+`
+    const left =
+      typeof option.parentheses === "string"
+        ? option.parentheses
+        : option.parentheses.left;
+    const right =
+      typeof option.parentheses === "string"
+        ? option.parentheses
+        : option.parentheses.right;
+    return `${left}(${str}(${right}))${repeater}`;
   }
 
   throw new Error("Invalid option: " + option);
 };
 
+export const zeroOrMore = (str: string) => orMore(str, 0);
+export const oneOrMore = (str: string) => orMore(str, 1);
+
 // 両端を囲む
-export const withAround = (str: string, parentheses: {
-  left: string;
-  right: string;
-} | string, option?: {
-  parenthesesQuantity: "one" | "zeroOrMore" | "oneOrMore";
-}) => {
+export const withAround = (
+  str: string,
+  parentheses:
+    | {
+        left: string;
+        right: string;
+      }
+    | string,
+  option?: {
+    parenthesesQuantity: "one" | "zeroOrMore" | "oneOrMore";
+  }
+) => {
   const left = typeof parentheses === "string" ? parentheses : parentheses.left;
-  const right = typeof parentheses === "string" ? parentheses : parentheses.right;
+  const right =
+    typeof parentheses === "string" ? parentheses : parentheses.right;
   const parenthesesQuantity = option?.parenthesesQuantity ?? "one";
 
   switch (parenthesesQuantity) {
@@ -62,27 +84,33 @@ export const withAround = (str: string, parentheses: {
 };
 
 // スペースを両端に挟んでOK
-export const maybeSpaceAround = (str: string) => withAround(str, META_REGEXP.SPACE, {
-  parenthesesQuantity: "zeroOrMore"
-});
+export const maybeSpaceAround = (str: string) =>
+  withAround(str, META_REGEXP.SPACE, {
+    parenthesesQuantity: "zeroOrMore",
+  });
 
 // ()で囲う
-export const withParentheses = (str: string) => withAround(str, {
-  left: "\\(",
-  right: "\\)"
-}, {
-  parenthesesQuantity: "one"
-});
+export const withParentheses = (str: string) =>
+  withAround(
+    str,
+    {
+      left: "\\(",
+      right: "\\)",
+    },
+    {
+      parenthesesQuantity: "one",
+    }
+  );
 
-export const withBarAround = (str: string) => withAround(str, META_REGEXP.BAR, {
-  parenthesesQuantity: "one"
-});
+export const withBarAround = (str: string) =>
+  withAround(str, META_REGEXP.BAR, {
+    parenthesesQuantity: "one",
+  });
 
 // CSV化
-export const csv = (exp: string) => `${exp}(${maybeSpaceAround(META_REGEXP.COMMA)}${exp})*`;
+export const csv = (exp: string) =>
+  `${exp}${orMore(`${maybeSpaceAround(META_REGEXP.COMMA)}${exp}`, 0)}`;
 
 // 挟む
-export const sand = (arg: {
-  center: string;
-  lr: string;
-}) => `(${arg.lr}${arg.center}${arg.lr})`;
+export const sand = (arg: { center: string; lr: string }) =>
+  `(${arg.lr}${arg.center}${arg.lr})`;
