4b20f82c82cf2bede2055409dd369c6d8266d8a6
nao <nao>
2023-09-20 00:51:30 +0900

    test

--- packages/chord-progression-markdown/src/matchChordProgressionMarkdown/matchChordProgressionMarkdown.spec.fixtures.ts
+++ packages/chord-progression-markdown/src/matchChordProgressionMarkdown/matchChordProgressionMarkdown.spec.fixtures.ts
@@ -1,98 +1,94 @@
 export const ok = [
-  // 基本的なコード
-  "|C|",
-  "|   C   |",
-  "|Cm|",
-  "|CM|",
-  "|CM7|",
-  "|Bbm|",
-  "|Bbm|",
-  "|Bbm7|",
-  "|Bbmb5|",
-  "|Bbdim7|",
-  "|Bbm7-5|",
-  "|Bbm13|",
-  "|Bbaug13|",
-  "|Bb7(9,13)|",
-  "|Bb7-5(9,13)|",
-
-  // 分数コード
-  "|C|G/B|F/A|Em/G|",
-
-  // 改行コード&連続バー
-  `|C|C|C|C|
-    |C|C|C|C|`,
-
-  // csv
-  "|Bb7-5(9,13),C,C,C|C , C,C,C|",
-
-  // メタ情報あり
-  "|(key=C)C|Dm|G7|C|",
-  "|(key=C)C|(key=Eb)Fm7|Bb7|EbM7|",
-
-  // 長い
-  `
-  |Aadd9|F#m7|Dm9|Gaug|
-  |G6|Aadd9|F#m7|
-  
-  |Fm9|Bb7(9,13)|EbM9|Ab7b5|AbM7|
-  |Bm7|E9|Ebm9|Ab|Gaug7|
-  |DbM9|G7b9|CM9|FM7|
-  |EM7|E9|Eaug7|Aadd9|
-  `,
+  {
+    title: "基本的なコード",
+    values: [
+      "|C|",
+      "|   C   |",
+      "|Cm|",
+      "|CM|",
+      "|CM7|",
+      "|Bbm|",
+      "|Bbm|",
+      "|Bbm7|",
+      "|Bbmb5|",
+      "|Bbdim7|",
+      "|Bbm7-5|",
+      "|Bbm13|",
+      "|Bbaug13|",
+      "|Bb7(9,13)|",
+      "|Bb7-5(9,13)|",
+    ],
+  },
+  {
+    title: "分数コード",
+    values: ["|C|G/B|F/A|Em/G|"],
+  },
+  {
+    title: "改行コード＆連続バー",
+    values: [
+      `|C|C|C|C|
+      |C|C|C|C|`,
+    ],
+  },
+  {
+    title: "1小節に複数のコード",
+    values: ["|Bb7-5(9,13),C,C,C|C , C,C,C|"],
+  },
+  {
+    title: "メタ情報つき",
+    values: [
+      "|(A=B)C|",
+      "|(A=B)C|(D=E)F|",
+      "|(key=C)C|",
+      "|(key=C)C|Dm|G7|C|",
+      "|(key=C)C|(key=Eb)Fm7|Bb7|EbM7|",
+    ],
+  },
+  {
+    title: "長く複雑なコード進行",
+    values: [
+      `
+      |Aadd9|F#m7|Dm9|Gaug|
+      |G6|Aadd9|F#m7|
+      
+      |Fm9|Bb7(9,13)|EbM9|Ab7b5|AbM7|
+      |Bm7|E9|Ebm9|Ab|Gaug7|
+      |DbM9|G7b9|CM9|FM7|
+      |EM7|E9|Eaug7|Aadd9|
+      `,
+    ],
+  },
 ];
 
 export const ng = [
   {
     title: "無い",
-    value: "",
-  },
-  {
-    title: "空白",
-    value: " ",
+    values: ["", " "],
   },
   {
     title: "バーのみ",
-    value: "|",
-  },
-  {
-    title: "バーのみ(改行あり)",
-    value: "|\n|",
-  },
-  {
-    title: "バーのみ（2つ連続）",
-    value: "||",
+    values: [
+      "|",
+      "|\n|", //改行あり
+      "||", // 連続バー
+      " | | ", // 連続バー
+    ],
   },
   {
     title: "バーで閉じない",
-    value: "|C",
+    values: ["|C", "|C|C", "C|C|"],
   },
   {
-    title: "バーで閉じない",
-    value: "|C|C",
-  },
-  {
-    title: "メタ情報がキーバリュー形式でない",
-    value: "|(key=C=)C|",
-  },
-  {
-    title: "メタ情報がキーバリュー形式でない",
-    value: "|(keyC)C|",
-  },
-  {
-    title: "メタ情報の形式がおかしい",
-    value: "|(=)C|",
-  },
-  {
-    title: "メタ情報の形式がおかしい",
-    value: "|(key=)C|",
-  },
-  {
-    title: "メタ情報の形式がおかしい",
-    value: "|(=value)C|",
-  },
-  {
-    title: "メタ情報が２つある",
-    value: "|(key=C)(key=C)C|",
+    title: "メタ情報の形式不正",
+    values: [
+      "|(key=C)(key=C)C|",
+      "|(keyC)C|",
+      "|(key=)C|",
+      "|(key=C=)C|",
+      "|(=key=C)C|",
+      "|((key=C))C|",
+      "|(=)C|",
+      "|(=value)C|",
+    ],
   },
 ];
--- packages/chord-progression-markdown/src/matchChordProgressionMarkdown/matchChordProgressionMarkdown.spec.ts
+++ packages/chord-progression-markdown/src/matchChordProgressionMarkdown/matchChordProgressionMarkdown.spec.ts
@@ -3,11 +3,15 @@ import { matchChordExpressionMarkdown } from "./matchChordProgressionMarkdown";
 import { ng, ok } from "./matchChordProgressionMarkdown.spec.fixtures";
 
 describe("matchChordExpressionMarkdown", () => {
-  test.each(ok)("問題ない: %s", (target) => {
-    expect(matchChordExpressionMarkdown(target)).toBe(true);
+  test.each(ok)("問題ない: %o", (target) => {
+    target.values.forEach((value) => {
+      expect(matchChordExpressionMarkdown(value)).toBe(true);
+    });
   });
 
   test.each(ng)("問題あり: %o", (target) => {
-    expect(matchChordExpressionMarkdown(target.value)).toBe(false);
+    target.values.forEach((value) => {
+      expect(matchChordExpressionMarkdown(value)).toBe(false);
+    });
   });
 });
